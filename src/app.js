4845dd53ec33329a6a49ce474110f032136cf1b6
Marlon Villalona <iamvillalona@gmail.com>
2024-10-08 22:40:01 -0400

    add customers

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json()) //Parseador de bodies
 // Conexion de base de datos:
 mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME})
 
+const db = mongoose.connection;
+
+app.use('/customers', customerRoutes)
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
