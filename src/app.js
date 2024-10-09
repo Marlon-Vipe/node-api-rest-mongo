a20dca072dbe9d72a1eec35795782703a1a2ea29
Marlon Villalona <iamvillalona@gmail.com>
2024-10-08 21:30:08 -0400

    add db connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const { config } = require('dotenv')
 config()
 
 const customerRoutes = require('./routes/customers.routes')
+
+// Usamos express para los middlewares
 const app = express();
+app.use(bodyParser.json()) //Parseador de bodies
+
+// Conexion de base de datos:
+mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME})
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
